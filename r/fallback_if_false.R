0408c4679c95f3119eb5e0700f25d14739912ba6
Daniel Göller <61782325+LelViLamp@users.noreply.github.com>
2024-03-19 14:16:57 +0100

    changed order of parameters to work with pipes %>%

--- r/fallback_if_false.R
+++ r/fallback_if_false.R
@@ -6,18 +6,18 @@
 #' 
 #' @author LelViLamp
 #'
-#' @param test_fn The function that will be used to test the value.
 #' @param value The value to be tested and returned if the test function is TRUE.
+#' @param test_fn The function that will be used to test the value.
 #' @param fallback The value to be returned if the test function is FALSE.
 #'
 #' @return The value if the test function is TRUE, otherwise the fallback value.
 #' @export
 #'
 #' @examples
-#' fallback_if_false(is.numeric, 1, 42) # returns 1
-#' fallback_if_false(is.numeric, "oh dear, a string, let's panic", 42) # returns 42
-#' fallback_if_false(is.numeric, NULL, 42) # returns 42
-fallback_if_false <- function(test_fn, value, fallback) {
+#' fallback_if_false(1, is.numeric, 42) # returns 1
+#' fallback_if_false("oh dear, a string, let's panic", is.numeric, 42) # returns 42
+#' fallback_if_false(NULL, is.numeric, 42) # returns 42
+fallback_if_false <- function(value, test_fn, fallback) {
   if (test_fn(value) == TRUE)
     return(value)
   else
