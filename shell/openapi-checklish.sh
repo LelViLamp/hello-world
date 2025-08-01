37ad59bd61d71dfbc7c21ae2f8187daa43099636
Daniel Göller <61782325+LelViLamp@users.noreply.github.com>
2025-08-01 13:22:29 +0200

    feat(openapi-checklist): route enclosed in backticks
    Makes them be formatted as code

--- shell/openapi-checklish.sh
+++ shell/openapi-checklish.sh
@@ -28,8 +28,8 @@ echo "# API Endpoints Checklist"
 echo
 
 yq eval '.paths | to_entries | .[] | .key as $path | .value | to_entries | .[] | (.key | upcase) + " " + $path' "$YAML_FILE" | \
-sed -E 's/^GET /- [ ] <span style="background-color: #61affe; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">GET<\/span> /' | \
-sed -E 's/^POST /- [ ] <span style="background-color: #49cc90; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">POST<\/span> /' | \
-sed -E 's/^PUT /- [ ] <span style="background-color: #fca130; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">PUT<\/span> /' | \
-sed -E 's/^PATCH /- [ ] <span style="background-color: #50e3c2; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">PATCH<\/span> /' | \
-sed -E 's/^DELETE /- [ ] <span style="background-color: #f93e3e; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">DELETE<\/span> /'
+sed -E 's/^GET (.*)$/- [ ] <span style="background-color: #61affe; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">GET<\/span> `\1`/' | \
+sed -E 's/^POST (.*)$/- [ ] <span style="background-color: #49cc90; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">POST<\/span> `\1`/' | \
+sed -E 's/^PUT (.*)$/- [ ] <span style="background-color: #fca130; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">PUT<\/span> `\1`/' | \
+sed -E 's/^PATCH (.*)$/- [ ] <span style="background-color: #50e3c2; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">PATCH<\/span> `\1`/' | \
+sed -E 's/^DELETE (.*)$/- [ ] <span style="background-color: #f93e3e; color: white; padding: 2px 6px; border-radius: 3px; font-weight: bold;">DELETE<\/span> `\1`/'
